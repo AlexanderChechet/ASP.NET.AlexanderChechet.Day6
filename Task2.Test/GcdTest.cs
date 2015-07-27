f0f41e2416eea933e79b87a38c520013b205bdb2
AlexanderChechet <Chechetsasha95@gmail.com>
2015-07-27 10:27:58 +0300

    Day6Commit1

---
+++ Task2.Test/GcdTest.cs
@@ -0,0 +1,297 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2.Test
+{
+    [TestClass]
+    public class GcdTest
+    {
+        [TestMethod]
+        public void SimpleEuclidGcdTimeTest()
+        {
+            int a = 3;
+            int b = 7;
+            long period;
+            
+            int result = Gcd.EuclidGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdTimeTest()
+        {
+            int a = 36;
+            int b = 21;
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdNullTimeTest()
+        {
+            int a = 0;
+            int b = 1;
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1 ,result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdFromOneTimeTest()
+        {
+            int a = 1;
+            int b = 5;
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdThreeArgsTimeTest()
+        {
+            int a = 100;
+            int b = 5;
+            int c = 10;
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, a, b, c);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdManyArgsTimeTest()
+        {
+            int[] array = new[] {9, 15, 21, 33, 99, 102};
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, array);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SimpleSteinGcdTimeTest()
+        {
+            int a = 3;
+            int b = 7;
+            long period;
+
+            int result = Gcd.SteinGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdTimeTest()
+        {
+            int a = 36;
+            int b = 21;
+            long period;
+
+            int result = Gcd.SteinGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdNullTimeTest()
+        {
+            int a = 0;
+            int b = 1;
+            long period;
+
+            int result = Gcd.SteinGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdFromOneTimeTest()
+        {
+            int a = 1;
+            int b = 5;
+            long period;
+
+            int result = Gcd.SteinGcd(out period, a, b);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdThreeArgsTimeTest()
+        {
+            int a = 100;
+            int b = 5;
+            int c = 10;
+            long period;
+
+            int result = Gcd.SteinGcd(out period, a, b, c);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdManyArgsTimeTest()
+        {
+            int[] array = new[] { 9, 15, 21, 33, 99, 102 };
+            long period;
+
+            int result = Gcd.EuclidGcd(out period, array);
+            Debug.WriteLine(period);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SimpleEuclidGcdWithoutTimeTest()
+        {
+            int a = 3;
+            int b = 7;
+
+            int result = Gcd.EuclidGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdWithoutTimeTest()
+        {
+            int a = 36;
+            int b = 21;
+
+            int result = Gcd.EuclidGcd(a, b);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdNullWithoutTimeTest()
+        {
+            int a = 0;
+            int b = 1;
+
+            int result = Gcd.EuclidGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdFromOneWithoutTimeTest()
+        {
+            int a = 1;
+            int b = 5;
+
+            int result = Gcd.EuclidGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdThreeArgsWithoutTimeTest()
+        {
+            int a = 100;
+            int b = 5;
+            int c = 10;
+
+            int result = Gcd.EuclidGcd(a, b, c);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void EuclidGcdManyArgsWithoutTimeTest()
+        {
+            int[] array = new[] { 9, 15, 21, 33, 99, 102 };
+
+            int result = Gcd.EuclidGcd(array);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SimpleSteinGcdWithoutTimeTest()
+        {
+            int a = 3;
+            int b = 7;
+
+            int result = Gcd.SteinGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdWithoutTimeTest()
+        {
+            int a = 36;
+            int b = 21;
+
+            int result = Gcd.SteinGcd(a, b);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdNullWithoutTimeTest()
+        {
+            int a = 0;
+            int b = 1;
+
+            int result = Gcd.SteinGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdFromOneWithoutTimeTest()
+        {
+            int a = 1;
+            int b = 5;
+
+            int result = Gcd.SteinGcd(a, b);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdThreeArgsWithoutTimeTest()
+        {
+            int a = 100;
+            int b = 5;
+            int c = 10;
+
+            int result = Gcd.SteinGcd(a, b, c);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void SteinGcdManyArgsWithoutTimeTest()
+        {
+            int[] array = new[] { 9, 15, 21, 33, 99, 102 };
+
+            int result = Gcd.EuclidGcd(array);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ Task2/Gcd.cs
@@ -0,0 +1,158 @@
+﻿using System.Diagnostics;
+
+namespace Task2
+{
+    public static class Gcd
+    {
+        #region Public Euclid methods
+        public static int EuclidGcd(int a, int b)
+        {
+            return GetEuclidGcd(a, b);
+        }
+
+        public static int EuclidGcd(int a, int b, int c)
+        {
+            return GetEuclidGcd(GetEuclidGcd(a, b), c);
+        }
+
+        public static int EuclidGcd(params int[] args)
+        {
+            int result = args[0];
+            for (int i = 1; i < args.Length; i++)
+                result = EuclidGcd(result, args[i]);
+            return result;
+        }
+
+        public static int EuclidGcd(out long time, int a, int b)
+        {
+            return GetEuclidGcd(out time, a, b);
+        }
+
+        public static int EuclidGcd(int a, int b, int c, out long time)
+        {
+            return GetEuclidGcd(out time, GetEuclidGcd(out time, a, b), c);
+        }
+
+        public static int EuclidGcd(out long time, params int[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            int result = args[0];
+            for (int i = 1; i < args.Length; i++)
+                result = EuclidGcd(out time, result, args[i]);
+            stopWatch.Stop();
+            time = stopWatch.ElapsedTicks;
+            return result;
+        }
+        #endregion
+
+        #region Public Stein methods
+        public static int SteinGcd(int a, int b)
+        {
+            return GetSteinGcd(a, b);
+        }
+
+        public static int SteinGcd(int a, int b, int c)
+        {
+            return GetSteinGcd(GetSteinGcd(a, b), c);
+        }
+
+        public static int SteinGcd(params int[] args)
+        {
+            int result = args[0];
+            for (int i = 1; i < args.Length; i++)
+                result = SteinGcd(result, args[i]);
+            return result;
+        }
+
+        public static int SteinGcd(out long time, int a, int b)
+        {
+            return GetSteinGcd(out time, a, b);
+        }
+
+        public static int SteinGcd(out long time, int a, int b, int c)
+        {
+            return GetSteinGcd(out time, GetSteinGcd(out time, a, b), c);
+        }
+
+        public static int SteinGcd(out long time, params int[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            int result = args[0];
+            for (int i = 1; i < args.Length; i++)
+                result = SteinGcd(out time, result, args[i]);
+            stopWatch.Stop();
+            time = stopWatch.ElapsedTicks;
+            return result;
+        }
+        #endregion
+
+        #region Private methods
+        private static int GetEuclidGcd(out long time, int a, int b)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            int result = GetEuclidGcd(a, b);
+            time = stopWatch.ElapsedTicks;
+            return result;
+        }
+
+        private static int GetEuclidGcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        private static int GetSteinGcd(out long time, int a, int b)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            int result = GetSteinGcd(a, b);
+            stopWatch.Stop();
+            time = stopWatch.ElapsedTicks;
+            return result;
+        }
+
+        private static int GetSteinGcd(int a, int b)
+        {
+            int shift;
+
+            if (a == 0) return b;
+            if (b == 0) return a;
+
+            for (shift = 0; ((a | b) & 1) == 0; ++shift)
+            {
+                a >>= 1;
+                b >>= 1;
+            }
+
+            while ((a & 1) == 0)
+                a >>= 1;
+
+            do
+            {
+                while ((b & 1) == 0)
+                {
+                    b >>= 1;
+                }
+
+                if (a > b)
+                {
+                    Swap(ref a, ref b);
+                }
+                b = b - a;
+            } while (b != 0);
+            return a << shift;
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        #endregion
+    }
+}
